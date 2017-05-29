3ce131d3b944b8b37ed02855c14ac126990ceda8
Simon Erkelens <simonerkelens@silverstripe.com>
2017-05-29 18:35:48 +1200

    Blabla, I did stuff to make stuff  work.

--- mysite/_config.php
+++ mysite/_config.php
@@ -8,7 +8,5 @@ $project = 'mysite';
 
 require_once('conf/ConfigureFromEnv.php');
 
-//Database::setQueryLog('/data/logs/querylogs/queries-'.time().'.log');
-
 // Set the site locale
 i18n::set_locale('en_US');
